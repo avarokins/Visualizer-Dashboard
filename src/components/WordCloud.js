37b2d9d4852922a65a1a8776462dddc336a2fa93
Avarokin Saini <saini7@purdue.edu>
2020-04-18 13:01:48 +0530

    Clean slate.

---
+++ src/components/WordCloud.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import data from '../data/jsondata.json'
+
+class WordCount extends Component {
+  render () {
+    return (
+      <div>
+        <h2>WordCount Test</h2>
+      </div>
+    )
+  }
+}
+
+export default WordCount
